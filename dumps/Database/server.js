857b54e35da442348a8540a6a948da2639e4ca82
DinhDuyen2810 <166526709+DinhDuyen2810@users.noreply.github.com>
2025-05-21 10:06:41 +0700

    Update server.js

--- dumps/Database/server.js
+++ dumps/Database/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors'); // Import cors
 
 const app = express();
 const port = 3000;
-
 
 const connection = mysql.createConnection({
   host: 'gvdp4.h.filess.io',
@@ -14,6 +12,11 @@ const connection = mysql.createConnection({
   database: 'shopdb_lookwordno'
 });
 
+/////////////////////////////////////////////////
+const table = 'Products';
+const query = `SELECT * FROM Products`;
+/////////////////////////////////////////////////
+
 connection.connect(err => {
   if (err) {
     console.error('Lỗi kết nối:', err);
@@ -22,14 +25,13 @@ connection.connect(err => {
   console.log('Kết nối MySQL thành công!');
 });
 
-app.use(cors()); // Sử dụng middleware cors
 app.get('/', (req, res) => {
   res.send('Chào mừng đến với API ShopDB!');
 });
 
-// API lấy danh sách sản phẩm
-app.get('/api/products', (req, res) => {
-  connection.query('SELECT * FROM Products', (err, results) => {
+
+app.get(`/api/${table}`, (req, res) => {
+  connection.query(query, (err, results) => {
     if (err) {
       console.error('Lỗi truy vấn:', err);
       res.status(500).send('Lỗi truy vấn CSDL');
@@ -38,28 +40,7 @@ app.get('/api/products', (req, res) => {
     }
   });
 });
-// API lấy chi tiết 1 sản phẩm theo ID
-app.get('/api/products/:id', (req, res) => {
-    const productId = req.params.id; // Lấy ID từ URL (ví dụ: /api/products/1)
-    // Chú ý: Sử dụng prepared statement hoặc sanitization để tránh SQL Injection
-    const query = 'SELECT * FROM Products WHERE product_id = ?';
-    connection.query(query, [productId], (err, results) => {
-        if (err) {
-            console.error('Lỗi truy vấn chi tiết sản phẩm:', err);
-            res.status(500).send('Lỗi truy vấn CSDL');
-        } else {
-            // results là một mảng, nhưng nếu product_id là khóa chính thì chỉ có 0 hoặc 1 kết quả
-            if (results.length > 0) {
-                res.json(results[0]); // Trả về đối tượng sản phẩm đầu tiên (và duy nhất)
-            } else {
-                res.status(404).send('Không tìm thấy sản phẩm'); // Trả về 404 nếu không tìm thấy
-            }
-        }
-    });
-});
 
 app.listen(port, () => {
-  console.log(`Server đang chạy tại http://localhost:${port}`);
+  console.log(`http://localhost:${port}/api/${table}`);
 });
-
-// Truy cập http://localhost:3000/api/products
